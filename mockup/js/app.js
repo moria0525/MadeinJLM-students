554fb2941fff088fb85460711f3b33ce2dcbcc9e
alonshmilo <alonshmilo@gmail.com>
2016-03-20 17:20:23 +0200

    Uploading javascript to js folder
    Uploading javascript to js folder

---
+++ mockup/js/app.js
@@ -0,0 +1,81 @@
+
+var jlm = angular.module('jlm', ['ngRoute','ui.bootstrap']);
+
+jlm.config(function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'view/home.html',
+      controller: 'UserNotConnected'
+    }).
+    when('/not-us', {
+      templateUrl: 'view/not-us.html',
+      controller: 'UserNotConnected'
+    }).
+    when('/login', {
+      templateUrl: 'view/login.html',
+      controller: 'UserNotConnected'
+    }).
+    when('/forgot-password', {
+      templateUrl: 'view/forgot-password.html',
+      controller: 'UserNotConnected'
+    }).
+    /*
+	when('/profile', {
+      templateUrl: 'view/profile.html',
+      controller: 'UserConnected'
+    }).
+	*/
+    otherwise({
+      redirectTo: '/'
+    });
+	/*
+    when('/:countryId', {
+      templateUrl: 'home.html',
+      controller: 'CountryDetailCtrl'
+    }).
+	*/
+});
+
+
+
+jlm.controller('UserNotConnected', function ($scope, countries){
+  /*
+  countries.list(function(countries) {
+    $scope.countries = countries;
+  });
+  */
+});
+
+jlm.controller('UserConnected', function ($scope, $routeParams, countries){
+  /*
+  countries.find($routeParams.countryId, function(country) {
+    $scope.country = country;
+  });
+  */
+});
+
+jlm.factory('countries', function($http){
+    var fac = {};
+     
+    fac.users = ['John', 'James', 'Jake']; 
+     
+    return fac;
+  /*
+  return {
+    list: function (callback){
+      $http({
+        method: 'GET',
+        url: 'countries.json',
+        cache: true
+      }).success(callback);
+    },
+    find: function(id, callback){
+      $http({
+        method: 'GET',
+        url: 'country_' + id + '.json',
+        cache: true
+      }).success(callback);
+    }
+  };
+  */
+});

1b64fa555c1e0b16eb7dda8b64408e1e52963910
Barak Turgeman <info@thinksmart.co.il>
2016-04-05 23:51:22 +0300

    Delete unnecessary code

--- mockup/core/class/Student.php
+++ mockup/core/class/Student.php
@@ -1,22 +1,12 @@
 <?php
-
-    class Student extends User {
-        public function __construct()
-        {
-            parent::__construct();
-
-            /*
-            //Add database credentials
-			$this->config->database->host = 'localhost';
-            $this->config->database->user = 'root';
-            $this->config->database->password = '';
-            $this->config->database->name = 'jlm'; //Database name
-			*/
-			
-			$this->config->userTableName = 'student';
-			
-            // Start object construction
-            $this->start();
-        }
-    }
+class Student extends User {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->config->userTableName = 'student';
+		
+		// Start object construction
+		$this->start();
+	}
+}
 ?>

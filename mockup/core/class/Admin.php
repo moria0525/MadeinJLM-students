f0babd629414dbca38298c8afdb68214fd4a12b9
Barak Turgeman <info@thinksmart.co.il>
2016-04-05 23:50:07 +0300

    Delete unnecessary code

--- mockup/core/class/Admin.php
+++ mockup/core/class/Admin.php
@@ -3,20 +3,11 @@
     class Admin extends User {
         public function __construct()
         {
-            parent::__construct();
-
-            /*
-            //Add database credentials
-			$this->config->database->host = 'localhost';
-            $this->config->database->user = 'root';
-            $this->config->database->password = '';
-            $this->config->database->name = 'jlm'; //Database name
-			*/
+        	parent::__construct();
+		$this->config->userTableName = 'admin';
 			
-			$this->config->userTableName = 'admin';
-			
-            // Start object construction
-            $this->start();
+        	// Start object construction
+            	$this->start();
         }
     }
 ?>

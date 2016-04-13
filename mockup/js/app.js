3674284f3520a7ef89b68968b99729fe077f35fa
Dor-H <dorharel1@hotmail.com>
2016-04-13 15:58:20 +0300

    checkbox for term of use befor sign in

--- mockup/js/app.js
+++ mockup/js/app.js
@@ -14,6 +14,7 @@ jlm.config(function($routeProvider) {
             templateUrl: 'view/not-us.html',
             // controller: 'UserNotConnected',
         }).
+
         when('/login', {
             templateUrl: 'view/login.html',
             controller: 'UserNotConnected',
@@ -26,14 +27,15 @@ jlm.config(function($routeProvider) {
             templateUrl: 'view/forgot-password.html',
             controller: 'UserNotConnected',
         }).
+    when('/termOfUse', {
+        templateUrl: 'view/termOfUse.html',
+        controller: 'UserNotConnected',
+    }).
         when('/profile', {
             templateUrl: 'view/profile.html',
             controller: 'UserConnected'
         }).
-        when('/termOfUse', {
-        templateUrl: 'view/termOfUse.html',
 
-        }).
         otherwise({
             redirectTo: '/'
         });

4d05cef5efe8a919959169d99d3d584991cbcf55
Think-Smart <info@thinksmart.co.il>
2016-04-17 19:01:02 +0300

    Update server side reset password

--- mockup/api/General/index.php
+++ mockup/api/General/index.php
@@ -23,7 +23,7 @@ class API_General extends API {
     }
     public function GetSkills() {
 		$this->start_db();
-		$sql = 'SELECT * FROM  skills';
+		$sql = 'SELECT * FROM  `skills`';
 		print_r($this->db->getQuery($sql));
 		die();
     }

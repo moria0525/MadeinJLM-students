ccb48c7356a1cfc2a415b2a6534f123d53c9ef47
Think-Smart <info@thinksmart.co.il>
2016-05-02 21:26:54 +0300

    remove pull.php not necessary

--- pull.php
+++
@@ -1,8 +0,0 @@
-<html style="background: #101241;">
-    <body>
-    <?php
-    $output = shell_exec('git pull origin master');
-    echo "<pre style=\"width: 570px;margin: 90px auto;color: #FFF;font-size: 20px;\">$output</pre>";
-    ?>
-    </body>
-</html>

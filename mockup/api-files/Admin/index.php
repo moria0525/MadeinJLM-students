bd31bac0a51e5ca6d3ce405f90c9efc6592938e0
moria0525 <moria0525@gmail.com>
2016-05-29 19:52:00 +0300

    work on admin

--- mockup/api-files/Admin/index.php
+++ mockup/api-files/Admin/index.php
@@ -37,4 +37,93 @@ class API_Admin extends API {
         
         return $return_arr;
     }
+	public function logOut() {
+        
+        $admin = new Admin();
+        
+        $admin->logout();
+        return ['status' => "success"]; 
+    }
+	
+	 public function resetPassword() {
+        if (isset($_POST['Email']))
+            $email = $_POST['Email'];
+        else $email = '';
+        
+        $admin = new Admin();
+
+        //Login with credentials
+        if ($data = $admin->resetPassword($email)) {
+			// send email to $data->Email whith the confirmation $data->Confirmation
+			sendMail::send($data->Email, 'Reset your password','Hello ' . $user->first_name . ' please click on the link at the bottom to reset your password.<br>click <a href="http://job.madeinjlm.org/MadeinJLM-students/mockup/#admin-new-password/' . $data->Confirmation . '">here</a>');
+			$return_arr = ['status' => "success"];
+		} else {
+            //Display Errors
+            $errors = array();
+            foreach($admin->log->getErrors() as $err){
+                $errors[] = $err;
+            }
+            $return_arr =  ['status' => "error",'errors' => $errors];
+		}
+        
+        return $return_arr;
+    }
+    public function newPassword() {
+        if (isset($_POST['hash']))
+            $hash = $_POST['hash'];
+        else $hash = '';
+        
+        if (isset($_POST['newPass']))
+            $newPass = $_POST['newPass'];
+        else $newPass = '';
+        
+        $admin = new Admin();
+		
+        //Login with credentials
+        if ($data = $admin->newPassword($hash,$newPass)) {
+			$return_arr = ['status' => "success",$newPass];
+		} else {
+            //Display Errors
+            $errors = array();
+            foreach($admin->log->getErrors() as $err){
+                $errors[] = $err;
+            }
+            $return_arr =  ['status' => "error",'errors' => $errors];
+		}
+        
+        return $return_arr;
+    }
+    public function changePassword() {
+        $admin = new Admin();
+        
+        if ($data = $admin->update($_POST)) {
+            $return_arr = ['status' => "success"];
+        } else {
+             $errors = array();
+            foreach($admin->log->getErrors() as $err){
+                $errors[] = $err;
+            }
+            $return_arr =  ['status' => "error",'errors' => $errors];
+        }
+        return $return_arr;
+
+    }
+	  public function update() {
+        $admin = new Admin();
+        
+        if ($data = $admin->update($_POST)) {
+            $return_arr = ['status' => "success"];
+        } else {
+             $errors = array();
+            foreach($admin->log->getErrors() as $err){
+                $errors[] = $err;
+            }
+            $return_arr =  ['status' => "error",'errors' => $errors];
+        }
+        return $return_arr;
+    }
+	
+	
+	
+	
 }
--- mockup/js/app.js
+++ mockup/js/app.js
@@ -40,6 +40,26 @@ jlm.config(function ($routeProvider, $locationProvider) {
             templateUrl: 'view/change-password.html',
             controller: 'UserConnected'
         }).
+		when('/admin-login', {
+            templateUrl: 'view/admin-login.html',
+            controller: 'AdminNotConnected'
+        }).
+		when('/admin-reset-password', {
+            templateUrl: 'view/admin-reset-password.html',
+            controller: 'AdminNotConnected'
+        }).
+		when('/admin-new-password/:hash', {
+            templateUrl: 'view/admin-new-password.html',
+            controller: 'AdminNotConnected'
+        }).
+		when('/admin-change-password', {
+            templateUrl: 'view/admin-change-password.html',
+            controller: 'AdminConnected'
+        }).
+		when('/admin', {
+            templateUrl: 'view/admin.html',
+            controller: 'AdminConnected'
+        }).
         otherwise({
             redirectTo: '/'
         });
@@ -95,6 +115,8 @@ jlm.controller('UserNotConnected', function ($scope, $http, $routeParams, $locat
         });
     };
     
+
+	
     $scope.resetPassword = function () {
         student.resetPassword($scope.data.resetPassword.Email).success(function (data) {
             if (data.status === 'error') {
@@ -117,7 +139,53 @@ jlm.controller('UserNotConnected', function ($scope, $http, $routeParams, $locat
 			});
 		}
     };
-    
+});
+
+    //admin area
+jlm.controller('AdminNotConnected', function ($scope, $http, $routeParams, $location, admin, $rootScope) {
+	'use strict';
+	
+	admin.init().success(function (data) {
+        $rootScope.adminData = data;
+        if ($rootScope.adminData !== false) {
+            $location.path("/admin-login");
+        }
+    });
+		
+	$scope.adminlogin = function () {
+        admin.adminlogin($scope.data.adminlogin).success(function (data) {
+            if (data.status === 'error') {
+                $scope.alerts.adminlogin = {type: 'danger', msg: data.errors.join('<br>')};
+            } else {
+                $scope.alerts.adminlogin = {type: 'success', msg: 'Success'};
+                $location.path("/admin");
+            }
+        });
+    };
+	$scope.adminresetPassword = function () {
+        admin.adminresetPassword($scope.data.adminresetPassword.Email).success(function (data) {
+            if (data.status === 'error') {
+                $scope.alerts.adminresetPassword = {type: 'danger', msg: data.errors.join('<br>')};
+            } else {
+                $scope.alerts.adminresetPassword = {type: 'success', msg: 'A link was sent to your mail, you can reset your password with it'};
+            }
+        });
+    };
+	$scope.adminnewPassword = function () {
+		if ($scope.data.adminnewPassword.password !== $scope.data.adminnewPassword.password2) {
+			$scope.alerts.adminnewPassword = {type: 'danger', msg: 'Passwords does not match'};
+		} else {
+			student.adminnewPassword($routeParams.hash, {Password: $scope.data.adminnewPassword.password}).success(function (data) {
+				if (data.status === 'error') {
+					$scope.alerts.adminnewPassword = {type: 'danger', msg: data.errors.join('<br>')};
+				} else {
+					$scope.alerts.adminnewPassword = {type: 'success', msg: 'Password changed'};
+				}
+			});
+		}
+    };
+	
+	 
 });
 
 jlm.controller('UserConnected', function ($scope, $http, $routeParams, $location, student, general, $rootScope) {
@@ -151,8 +219,37 @@ jlm.controller('UserConnected', function ($scope, $http, $routeParams, $location
             }
         });
     };
-     
 });
+	//admin area
+	
+jlm.controller('AdminConnected', function ($scope, $http, $routeParams, $location, admin, general, $rootScope) {
+	'use strict';
+	admin.init().success(function (data) {
+        $rootScope.adminData = data;
+        if ($rootScope.adminData === false) {
+            $location.path("/admin-login");
+        }
+    });
+	$scope.adminlogOut = function () {
+        admin.adminlogOut().success(function (data) {
+            if (data.status === 'success') {
+                $location.path("/admin-login");
+            }
+        });
+    };
+	
+	$scope.adminchangePassword = function () {
+        admin.adminchangePassword($scope.data.adminchangePassword).success(function (data) {
+            if (data.status === 'error') {
+                $scope.alerts.adminchangePassword = {type: 'danger', msg: data.errors.join('<br>')};
+            } else {
+                $scope.alerts.adminchangePassword = {type: 'success', msg: 'Your password was change successfully'};
+            }
+        });
+    };
+	
+});
+
 jlm.controller('generalController', function ($scope, $rootScope, student) {
     "use strict";
     $scope.data = {};
@@ -180,6 +277,7 @@ jlm.controller('DropdownCtrl', function ($scope, $log) {
 
 	$scope.appendToEl = angular.element(document.querySelector('#dropdown-long-content'));
 });
+
 
 /* --- profile --- */
 jlm.directive('profilePicture', function () {
@@ -376,7 +474,7 @@ jlm.factory('general', ['$http', '$httpParamSerializerJQLike', function ($http, 
             }).success(function (data) {
                 return data;
             }).error(function () {return {}; });
-        },
+        }
 		/*
 		profileEditFormat: function () {
             return $http({
@@ -390,3 +488,88 @@ jlm.factory('general', ['$http', '$httpParamSerializerJQLike', function ($http, 
 		*/
     };
 }]);
+
+//admin
+
+jlm.factory('admin', ['$http', '$httpParamSerializerJQLike', function ($http, $httpParamSerializerJQLike) {
+    "use strict";
+    return {
+        init: function () {
+            return $http({
+                method  : 'POST',
+                url     : 'API/Admin',
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+				if (typeof data.status !== 'undefined' && data.status !== null) {
+					data.status = parseInt(data.status);
+				}
+				return data;
+            }).error(function () {return false; });
+        },
+        adminlogin: function (data) {
+            return $http({
+                method  : 'POST',
+                url     : 'API/Admin/Login',
+                data    : $httpParamSerializerJQLike(data),
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+                return data;
+            }).error(function () {return {'status': 'error', 'errors': 'Please try again later.'}; });
+        },
+		
+        adminlogOut: function () {
+            return $http({
+                method  : 'POST',
+                url     : 'API/Admin/logOut',
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+                return data;
+            }).error(function () {return {'status': 'error', 'errors': 'Please try again later.'}; });
+        },
+        adminresetPassword: function (email) {
+            return $http({
+                method  : 'POST',
+                url     : 'API/Admin/resetPassword',
+                data    : $httpParamSerializerJQLike({'Email': email}),  // pass in data as strings
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+                return data;
+            }).error(function () {return {'status': 'error', 'errors': 'Please try again later..'}; });
+        },
+        adminnewPassword: function (hash, newPass) {
+			return $http({
+                method  : 'POST',
+                url     : 'API/Admin/newPassword',
+                data    : $httpParamSerializerJQLike({'hash': hash, 'newPass': newPass}),  // pass in data as strings
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+                return data;
+            }).error(function () {return {'status': 'error', 'errors': 'Please try again later.'}; });
+        },
+        adminchangePassword: function (data) {
+            return $http({
+                method  : 'POST',
+                url     : 'API/Admin/changePassword',
+                data    : $httpParamSerializerJQLike(data),
+                headers : { 'Content-Type':  'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+                return data;
+            }).error(function () {return {'status': 'error', 'errors': 'Please try again later.'}; });
+        },
+        adminupdate: function (data) {
+            return $http({
+                method  : 'POST',
+                url     : 'API/Admin/update',
+                data    : $httpParamSerializerJQLike(data),
+                headers : { 'Content-Type':  'application/x-www-form-urlencoded' }
+            }).success(function (data) {
+                return data;
+            }).error(function () {return {'status': 'error', 'errors': 'Please try again later.'}; });
+        }
+       
+    };
+}]);
+
+
+
+
